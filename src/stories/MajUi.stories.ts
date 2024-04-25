2ff7cfedc7ab8adf993d40399ae174570dc5390d
akhiljozph <ajpulluvelil@gmail.com>
2024-04-25 14:12:08 +0530

    MAJ-53 - Updates for MajUi story.

--- src/stories/MajUi.stories.ts
+++ src/stories/MajUi.stories.ts
@@ -21,6 +21,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
     args: {
         color: '#ffc40c',
-        backgroundColor: '#000000'
+        backgroundColor: '#000000',
     }
 };
