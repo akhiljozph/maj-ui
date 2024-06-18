70f2b46eb650a46a3e04579b21bdccd7d493a9ed
akhiljoseph <ajpulluvelil@gmail.com>
2024-06-18 09:46:52 +0530

    MAJ-51 - Updates for formatting.

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,16 @@ import Majui from './components/maj-ui/MajUi'
 import MajButton from './components/maj-button/MajButton'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Majui backgroundColor="#644040" color="#ffad0d" />
+    <React.StrictMode>
+        <Majui backgroundColor="#644040" color="#ffad0d" />
 
-    <div className='maj-ui-desc'>
-      MAJui offers a comprehensive suite of free UI tools to help you ship
-      new features faster. Start with MAJui, our fully loaded component library or
-      bring your own design system to our production-ready components.
-    </div>
-    <div className="maj-btn-doc">
-      <MajButton link={'https://127.0.0.1:8090/'}>Discover our components</MajButton>
-    </div>
-  </React.StrictMode>,
+        <div className='maj-ui-desc'>
+            MAJui offers a comprehensive suite of free UI tools to help you ship
+            new features faster. Start with MAJui, our fully loaded component library or
+            bring your own design system to our production-ready components.
+        </div>
+        <div className="maj-btn-doc">
+            <MajButton link={'https://127.0.0.1:8090/'}>Discover our components</MajButton>
+        </div>
+    </React.StrictMode>,
 )
