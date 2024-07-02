0f9a9b7fe7a8bed2272659f98f61461a6260b825
akhiljoseph <ajpulluvelil@gmail.com>
2024-07-02 11:32:39 +0530

    MAJ-71 - New pages added.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import ExpLanding from './modules/expense-manager/exp-landing/ExpLanding'
 
-import Majui from './components/maj-ui/MajUi'
-import MajButton from './components/maj-button/MajButton'
+// import Majui from './components/maj-ui/MajUi'
+// import MajButton from './components/maj-button/MajButton'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Majui backgroundColor="#644040" color="#ffad0d" />
+        {/* <Majui backgroundColor="#644040" color="#ffad0d" />
 
         <div className='maj-ui-desc'>
             MAJui offers a comprehensive suite of free UI tools to help you ship
@@ -17,6 +18,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </div>
         <div className="maj-btn-doc">
             <MajButton link={'https://127.0.0.1:8090/'}>Discover our components</MajButton>
-        </div>
+        </div> */}
+
+        <ExpLanding />
+
     </React.StrictMode>,
 )
