c0c95dd5d12f05054ae47af1c67fd65c4cc244c2
akhiljozph <ajpulluvelil@gmail.com>
2024-08-02 11:36:37 +0530

    MAJ-3 - Commit to test the master-branch-ruleset.

---
+++ src/stories/MajAvatar.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import MajAvatar from "../components/maj-avatar/MajAvatar";
+
+const meta = {
+  title: "MajUI/MajAvatar",
+  component: MajAvatar,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: { onClick: fn() },
+} satisfies Meta<typeof MajAvatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
+
+
+
