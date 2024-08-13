4e982585f21f880daa9c35aa94da9dc4713bf862
akhiljozph <ajpulluvelil@gmail.com>
2024-08-13 20:13:01 +0530

    MAJ-26 - Updates based on review comments.

--- src/components/maj-button/MajButton.tsx
+++ src/components/maj-button/MajButton.tsx
@@ -6,17 +6,17 @@ const MajButton: React.FC<MajButtonProps> = (props: MajButtonProps) => {
   return (
     <button
       className={
-        props.size === "large"
+        props?.size === "large"
           ? `maj-btn maj-btn-lg maj-${props.variant}-btn`
-          : props.size === "small"
+          : props?.size === "small"
             ? `maj-btn maj-btn-sm maj-${props.variant}-btn `
             : `maj-btn maj-${props.variant}-btn`
       }
-      disabled={props.disabled}
-      onClick={props.onClick}
-      type={props.type}
+      disabled={props?.disabled}
+      onClick={props?.onClick}
+      type={props?.type}
     >
-      {props.text}
+      {props?.text}
     </button>
   );
 };
