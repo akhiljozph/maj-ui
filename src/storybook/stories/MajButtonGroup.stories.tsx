208d8cf5b80f02c0523a810893f73309dfd213fe
akhiljozph <ajpulluvelil@gmail.com>
2024-09-10 10:57:35 +0530

    MAJ-56 - Added new story for button group.

---
+++ src/storybook/stories/MajButtonGroup.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MajButtonGroup from "../../components/maj-button-group/MajButtonGroup";
+
+const meta: Meta<typeof MajButtonGroup> = {
+      title: "MajUi/MajButtonGroup",
+      component: MajButtonGroup,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajButtonGroup>;
+
+export const Primary: Story = {
+      args: {
+      },
+};
