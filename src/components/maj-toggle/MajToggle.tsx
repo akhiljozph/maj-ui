924d69f14d1c57b63e1a5e20071e1ad201f2b5d7
akhiljozph <ajpulluvelil@gmail.com>
2024-10-05 20:14:05 +0530

    MAJ-80 - Updates for maj-toggle component.

--- src/components/maj-toggle/MajToggle.tsx
+++ src/components/maj-toggle/MajToggle.tsx
@@ -1,14 +1,26 @@
 import { MajToggleProps } from "./MajToggle.types";
 
 import "./MajToggle.scss";
+import { useState } from "react";
 
 const MajToggle: React.FC<MajToggleProps> = (props: MajToggleProps) => {
+    const [selectedToggle, setSelectedToggle] = useState<number>(1);
     return (
         <section className="maj-toggle">
-            <button className="maj-toggle-btn maj-toggle-one">
+            <button
+                className={
+                    "maj-toggle-btn maj-toggle-one " + (selectedToggle === 1 ? "active" : "")
+                }
+                onClick={() => setSelectedToggle(1)}
+            >
                 <label className="maj-toggle-label">{props.toggleOneLabel}</label>
             </button>
-            <button className="maj-toggle-btn maj-toggle-two">
+            <button
+                className={
+                    "maj-toggle-btn maj-toggle-two " + (selectedToggle === 2 ? "active" : "")
+                }
+                onClick={() => setSelectedToggle(2)}
+            >
                 <label className="maj-toggle-label">{props.toggleTwoLabel}</label>
             </button>
         </section>
