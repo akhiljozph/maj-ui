bdbd2190dae27262bbeb662867ad309ea43239c5
akhiljozph <ajpulluvelil@gmail.com>
2024-10-06 11:51:37 +0530

    MAJ-81 - Updates for maj-toggle component.

--- src/components/maj-toggle/MajToggle.tsx
+++ src/components/maj-toggle/MajToggle.tsx
@@ -5,24 +5,43 @@ import { useState } from "react";
 
 const MajToggle: React.FC<MajToggleProps> = (props: MajToggleProps) => {
     const [selectedToggle, setSelectedToggle] = useState<number>(1);
+    const [isToggled, setIsToggled] = useState<boolean>(false);
     return (
         <section className="maj-toggle">
-            <button
-                className={
-                    "maj-toggle-btn maj-toggle-one " + (selectedToggle === 1 ? "active" : "")
-                }
-                onClick={() => setSelectedToggle(1)}
-            >
-                <label className="maj-toggle-label">{props.toggleOneLabel}</label>
-            </button>
-            <button
-                className={
-                    "maj-toggle-btn maj-toggle-two " + (selectedToggle === 2 ? "active" : "")
-                }
-                onClick={() => setSelectedToggle(2)}
-            >
-                <label className="maj-toggle-label">{props.toggleTwoLabel}</label>
-            </button>
+            {props.toggleOneLabel && props.toggleTwoLabel ? (
+                <>
+                    <button
+                        className={
+                            "maj-toggle-btn maj-toggle-one " +
+                            (selectedToggle === 1 ? "maj-toggle-active" : "")
+                        }
+                        onClick={() => setSelectedToggle(1)}
+                    >
+                        <label className="maj-toggle-label">{props?.toggleOneLabel}</label>
+                    </button>
+                    <button
+                        className={
+                            "maj-toggle-btn maj-toggle-two " +
+                            (selectedToggle === 2 ? "maj-toggle-active" : "")
+                        }
+                        onClick={() => setSelectedToggle(2)}
+                    >
+                        <label className="maj-toggle-label">{props?.toggleTwoLabel}</label>
+                    </button>
+                </>
+            ) : (
+                <button
+                    className={
+                        "maj-default-toggle " +
+                        (isToggled ? "toggle-end-align" : "toggle-start-align")
+                    }
+                    onClick={() => {
+                        setIsToggled(!isToggled);
+                    }}
+                >
+                    <span className="maj-toggle-action"></span>
+                </button>
+            )}
         </section>
     );
 };
