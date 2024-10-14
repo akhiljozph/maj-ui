95fb8d0d771f1445ccbc70d179003a5c06a64217
akhiljozph <ajpulluvelil@gmail.com>
2024-10-14 10:54:52 +0530

    MAJ-93 - Added new component.

---
+++ src/storybook/stories/MajRadioGroup.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MajRadioGroup from "../../components/maj-radio-group/MajRadioGroup";
+
+const meta: Meta<typeof MajRadioGroup> = {
+    title: "MajUi/MajRadioGroup",
+    component: MajRadioGroup,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajRadioGroup>;
+
+export const Primary: Story = {
+    args: {
+        name: "primary",
+        radioLists: [
+            "2 Wheeler",
+            "4 Wheeler",
+            "6 Wheeler"
+        ]
+    },
+};
