0f6c43316d70150af50900a1b61669acc648d1d7
akhiljozph <ajpulluvelil@gmail.com>
2024-10-15 10:12:11 +0530

    MAJ-96 - Formatting completed for maj-radio-group component.

--- src/components/maj-radio-group/MajRadioGroup.tsx
+++ src/components/maj-radio-group/MajRadioGroup.tsx
@@ -5,13 +5,14 @@ import "./MajRadioGroup.scss";
 const MajRadioGroup: React.FC<MajRadioGroupProps> = (props: MajRadioGroupProps) => {
     return (
         <section
-            className={ "radio-group-wrapper " +
+            className={
+                "radio-group-wrapper " +
                 (props?.alignment === "horizontal"
                     ? "horizontal-group"
                     : props?.alignment === "vertical"
-                        ? "vertical-group"
-                        : "horizontal-group"
-    )}
+                      ? "vertical-group"
+                      : "horizontal-group")
+            }
         >
             {props.radioLists.map((item: string) => (
                 <div>
