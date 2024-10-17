cac7b5b9ae15108086b65dd78d2dfc6edeb38ba2
akhiljozph <ajpulluvelil@gmail.com>
2024-10-17 10:45:14 +0530

    MAJ-98 - Updates for MajTextField.

--- src/components/maj-text-field/MajTextField.tsx
+++ src/components/maj-text-field/MajTextField.tsx
@@ -3,7 +3,11 @@ import { MajTextFieldProps } from "./MajTextField.types";
 import "./MajTextField.scss";
 
 const MajTextField: React.FC<MajTextFieldProps> = (props: MajTextFieldProps) => {
-    return <>maj-textfield works!</>;
+    return (
+        <section>
+            <textarea rows={props.rows} cols={props.cols}></textarea>
+        </section>
+    );
 };
 
 export default MajTextField;
