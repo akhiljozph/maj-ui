82b179b2d7dc1b1d57bee7c0e5b2e272154163c7
akhiljozph <ajpulluvelil@gmail.com>
2024-11-11 09:57:16 +0530

    MAJ-128 - Formatting completed.

--- src/components/maj-calendar/MajDatePicker.tsx
+++ src/components/maj-calendar/MajDatePicker.tsx
@@ -3,9 +3,13 @@ import { MajDatePickerProps } from "./MajDatePicker.type";
 const MajDatePicker: React.FC<MajDatePickerProps> = (props: MajDatePickerProps) => {
     return (
         <section>
-            <input type="date" name="maj-date-picker" id="maj-date-picker" />
+            <input
+                type="date"
+                name="maj-date-picker"
+                id="maj-date-picker"
+            />
         </section>
-    )
-}
+    );
+};
 
 export default MajDatePicker;
