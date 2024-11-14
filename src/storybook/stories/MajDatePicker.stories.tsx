8c08875ae3c37409ec571c7903eae559059b446b
akhiljozph <ajpulluvelil@gmail.com>
2024-11-14 21:26:07 +0530

    MAJ-141 - Story created date picker.

---
+++ src/storybook/stories/MajDatePicker.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MajDatePicker from "../../components/maj-date-picker/MajDatePicker";
+
+const meta: Meta<typeof MajDatePicker> = {
+    title: "MajUi/MajDatePicker",
+    component: MajDatePicker,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajDatePicker>;
+
+export const DatePicker: Story = {
+    args: {
+    },
+};
