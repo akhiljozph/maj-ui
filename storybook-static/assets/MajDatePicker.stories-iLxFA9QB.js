31df6a7a331701194d4261861087b23d97ae6fe7
akhiljozph <ajpulluvelil@gmail.com>
2024-11-16 23:22:26 +0530

    MAJ-143 - Storybook build.

---
+++ storybook-static/assets/MajDatePicker.stories-iLxFA9QB.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const m=p=>a.jsx("section",{children:a.jsx("input",{type:"date",name:"maj-date-picker",id:"maj-date-picker"})});m.__docgenInfo={description:"",methods:[],displayName:"MajDatePicker"};const u={title:"MajUi/MajDatePicker",component:m},e={args:{}},r={args:{}};var t,s,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var c,i,n;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {}
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const k=["DatePicker","DateTimePicker"];export{e as DatePicker,r as DateTimePicker,k as __namedExportsOrder,u as default};
--- storybook-static/components/maj-text-field/index.d.ts
+++ storybook-static/components/maj-text-field/index.d.ts
@@ -1,1 +1,1 @@
-export { default as MajTextField } from "./MajTextField";
+export { default as MajTextField } from './MajTextField';
