a3cf6d7598d110bd849ee6cf82467fbc31f99a36
akhiljozph <ajpulluvelil@gmail.com>
2024-11-29 22:46:45 +0530

    MAJ-151 - Storybook build.

--- storybook-static/components/maj-icon-button/MajIconButton.d.ts
+++ storybook-static/components/maj-icon-button/MajIconButton.d.ts
@@ -1,3 +1,3 @@
-import { MajIconButtonProps } from './MajIconButton.type';
+import { MajIconButtonProps } from "./MajIconButton.type";
 declare const MajIconButton: React.FC<MajIconButtonProps>;
 export default MajIconButton;
