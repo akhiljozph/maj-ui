d162bb22df266e08e4bd87b8893ca7eab47b7095
akhiljozph <ajpulluvelil@gmail.com>
2024-11-29 22:46:07 +0530

    MAJ-150 - Storybook build.

--- storybook-static/components/maj-button/MajButton.type.d.ts
+++ storybook-static/components/maj-button/MajButton.type.d.ts
@@ -1,14 +1,5 @@
-import { MouseEventHandler } from "../../../node_modules/react";
-type MajButtonVariants =
-    | "primary"
-    | "secondary"
-    | "success"
-    | "danger"
-    | "warning"
-    | "info"
-    | "light"
-    | "dark"
-    | "link";
+import { MouseEventHandler } from '../../../node_modules/react';
+type MajButtonVariants = "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" | "link";
 type MajButtonSizes = "small" | "medium" | "large";
 type MajButtonTypes = "button" | "submit" | "reset";
 export type MajButtonProps = {
