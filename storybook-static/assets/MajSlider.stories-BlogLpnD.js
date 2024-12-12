95241de42ded46b7330916226435f8732d2c4b11
akhiljozph <ajpulluvelil@gmail.com>
2024-12-12 16:38:05 +0530

    MAJ-134 - Added maj-slider component to storybook.

---
+++ storybook-static/assets/MajSlider.stories-BlogLpnD.js
@@ -0,0 +1,3 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"maj-slider works!"})});t.__docgenInfo={description:"",methods:[],displayName:"MajSlider"};const m={title:"MajUi/MajSlider",component:t},r={args:{}};var s,a,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const d=["Primary"];export{r as Primary,d as __namedExportsOrder,m as default};
