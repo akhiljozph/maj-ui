2af99ad1aeaf04a995c28f613c75a794878e9e2f
akhiljozph <ajpulluvelil@gmail.com>
2024-12-17 09:40:34 +0530

    MAJ-166 - New component added - Date Time Picker.

--- storybook-static/assets/MajTimePicker.stories-B-IcKlOW.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"maj-time-picker works!"})});t.__docgenInfo={description:"",methods:[],displayName:"MajTimePicker"};const c={title:"MajUi/MajTimePicker",component:t},r={args:{}};var a,s,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {}
-}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const n=["Primary"];export{r as Primary,n as __namedExportsOrder,c as default};
---
+++ storybook-static/assets/MajTimePicker.stories-Ce1erwgk.js
@@ -0,0 +1,3 @@
+import{j as r}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const o=()=>r.jsx(r.Fragment,{children:r.jsx("input",{type:"time",className:"time"})});o.__docgenInfo={description:"",methods:[],displayName:"MajTimePicker"};const c={title:"MajUi/MajTimePicker",component:o},e={args:{}};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {}
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const n=["Primary"];export{e as Primary,n as __namedExportsOrder,c as default};
