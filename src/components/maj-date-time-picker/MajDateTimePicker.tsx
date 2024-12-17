96194d5d0185aee515f720948c926eb29e89e50f
akhiljozph <ajpulluvelil@gmail.com>
2024-12-17 09:59:01 +0530

    MAJ-167 - Updates for MajDateTimePicker.

--- src/components/maj-date-time-picker/MajDateTimePicker.tsx
+++ src/components/maj-date-time-picker/MajDateTimePicker.tsx
@@ -2,13 +2,15 @@ import { MajDateTimePickerProps } from "./MajDateTimePicker.type";
 
 import "./MajDateTimePicker.scss";
 
-const MajDateTimePicker: React.FC<MajDateTimePickerProps> = () => {
+const MajDateTimePicker: React.FC<MajDateTimePickerProps> = (props: MajDateTimePickerProps) => {
     return (
         <>
             <input
                 type="datetime-local"
-                name=""
-                id=""
+                name="maj-date-time-picker"
+                id="maj-date-time-picker"
+                min={props?.minimumValue}
+                max={props?.maximumValue}
             />
         </>
     );
