b94678710dd45a8734dbbe5a066d470b44b813fd
akhiljozph <ajpulluvelil@gmail.com>
2024-12-21 23:01:16 +0530

    MAJ-171 - Storybook build.

--- storybook-static/assets/MajDatePicker.stories-CT93nywY.js
+++
@@ -1,3 +0,0 @@
-import{j as r}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const o=i=>r.jsx("section",{children:r.jsx("input",{type:"date",name:"maj-date-picker",id:"maj-date-picker"})});o.__docgenInfo={description:"",methods:[],displayName:"MajDatePicker"};const m={title:"MajUi/MajDatePicker",component:o},e={args:{}};var a,t,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {}
-}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const p=["Primary"];export{e as Primary,p as __namedExportsOrder,m as default};
