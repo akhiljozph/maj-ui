37be832105e7e666f2c8c60b418e903381be7915
akhiljozph <ajpulluvelil@gmail.com>
2025-01-08 23:06:45 +0530

    MAJ-186 - Added new icons.

---
+++ src/components/maj-icon/solid-icon/arrow-right-arrow-left/arrow-right-arrow-left.tsx
@@ -0,0 +1,12 @@
+import logo from "../../../../assets/icons/svg/solid/arrow-right-arrow-left.svg";
+
+const MajArrowRightArrowLeftIcon: React.FC<any> = () => {
+    return (
+        <img
+            src={logo}
+            alt="arrow-right-arrow-left"
+        />
+    );
+};
+
+export default MajArrowRightArrowLeftIcon;
