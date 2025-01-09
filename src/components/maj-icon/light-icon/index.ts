42367ebf5451cf92c594edc816a55ecce6aabfc8
akhiljozph <ajpulluvelil@gmail.com>
2025-01-09 21:48:44 +0530

    MAJ-188 - Added new icon - Arrow Down To Line.

--- src/components/maj-icon/light-icon/index.ts
+++ src/components/maj-icon/light-icon/index.ts
@@ -4,4 +4,5 @@ export * from "./angle-down";
 export * from "./angle-left";
 export * from "./angle-right";
 export * from "./angle-up";
+export * from "./arrow-down-to-line";
 export * from "./arrow-right-arrow-left";
---
+++ src/components/maj-icon/solid-icon/arrow-down-to-line/arrow-down-to-line.tsx
@@ -0,0 +1,12 @@
+import logo from "../../../../assets/icons/svg/solid/arrow-down-to-line.svg";
+
+const MajArrowDownToLineIcon: React.FC<any> = () => {
+    return (
+        <img
+            src={logo}
+            alt="arrow-down-to-line"
+        />
+    );
+};
+
+export default MajArrowDownToLineIcon;
