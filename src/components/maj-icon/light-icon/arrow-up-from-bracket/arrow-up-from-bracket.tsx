215002d5ad216ed3f440fa139c8d8df40e509136
akhiljozph <ajpulluvelil@gmail.com>
2025-01-13 23:29:41 +0530

    MAJ-194 - Formattign completed.

--- src/components/maj-icon/light-icon/arrow-up-from-bracket/arrow-up-from-bracket.tsx
+++ src/components/maj-icon/light-icon/arrow-up-from-bracket/arrow-up-from-bracket.tsx
@@ -2,12 +2,11 @@ import logo from "../../../../assets/icons/svg/light/arrow-up-from-bracket.svg";
 
 const MajArrowUpFromBracketIcon: React.FC<any> = () => {
     return (
-        
         <img
             src={logo}
             alt="arrow-right-arrow-left"
         />
-    )
+    );
 };
 
 export default MajArrowUpFromBracketIcon;
