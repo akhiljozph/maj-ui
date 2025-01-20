21687a0ec420aadf2a4fd945833029b87e45be59
akhiljozph <ajpulluvelil@gmail.com>
2025-01-20 21:23:23 +0530

    MAJ-198 - New icon (backward-step) added.

---
+++ src/components/maj-icon/solid-icon/backward-step/backward-step.tsx
@@ -0,0 +1,12 @@
+import logo from "../../../../assets/icons/svg/solid/backward-step.svg";
+
+const MajBackwardStepIcon: React.FC<any> = () => {
+    return (
+        <img
+            src={logo}
+            alt="backward-step"
+        />
+    );
+};
+
+export default MajBackwardStepIcon;
