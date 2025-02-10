f3f3bed0640035838e5ff2890edd8d52e7887b1d
Akhil Joseph <ajpulluvelil@gmail.com>
2025-02-10 22:50:57 +0530

    MAJ-231 - Added new icon (circle).

---
+++ src/components/maj-icon/solid-icon/circle/circle.tsx
@@ -0,0 +1,14 @@
+import logo from "../../../../assets/icons/svg/solid/circle.svg";
+import "../../assets/MajIcon.scss";
+
+const MajCircleIcon: React.FC<any> = () => {
+    return (
+        <img
+            className="maj-img"
+            src={logo}
+            alt="circle"
+        />
+    );
+};
+
+export default MajCircleIcon;
