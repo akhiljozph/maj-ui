ce540e1a964aa4eb4185e184fdfaaaa92ddf36f3
Akhil Joseph <ajpulluvelil@gmail.com>
2025-03-16 11:37:45 +0530

    MAJ-296 - Added new icon (square-linkedin).

---
+++ src/components/maj-icon/light-icon/square-linkedin/square-linkedin.tsx
@@ -0,0 +1,14 @@
+import logo from "../../../../assets/icons/svg/light/square-linkedin.svg";
+import "../../assets/MajIcon.scss";
+
+const MajSquareLinkedInIcon: React.FC<any> = () => {
+    return (
+        <img
+            className="maj-img"
+            src={logo}
+            alt="square-linkedin"
+        />
+    );
+};
+
+export default MajSquareLinkedInIcon;
