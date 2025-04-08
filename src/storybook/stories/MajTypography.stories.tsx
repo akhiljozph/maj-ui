0a618c154ba9dca4184b95111afa99b86b7d72fd
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-08 23:04:25 +0530

    MAJ-332 - Updates for maj-typography component.

--- src/storybook/stories/MajTypography.stories.tsx
+++ src/storybook/stories/MajTypography.stories.tsx
@@ -10,20 +10,56 @@ const meta: Meta<typeof MajTypography> = {
 export default meta;
 type Story = StoryObj<typeof MajTypography>;
 
-export const HeadingOne: Story = {};
+export const HeadingOne: Story = {
+    args: {
+        child: "Heading One",
+    },
+};
 
-export const HeadingTwo: Story = {};
+export const HeadingTwo: Story = {
+    args: {
+        child: "Heading Two",
+    },
+};
 
-export const HeadingThree: Story = {};
+export const HeadingThree: Story = {
+    args: {
+        child: "Heading Three",
+    },
+};
 
-export const HeadingFour: Story = {};
+export const HeadingFour: Story = {
+    args: {
+        child: "Heading Four",
+    },
+};
 
-export const HeadingFive: Story = {};
+export const HeadingFive: Story = {
+    args: {
+        child: "Heading Five",
+    },
+};
 
-export const HeadingSix: Story = {};
+export const HeadingSix: Story = {
+    args: {
+        child: "Heading Six",
+    },
+};
 
-export const SubScript: Story = {};
+export const SubScript: Story = {
+    args: {
+        child: "SubScript",
+    },
+};
 
-export const SuperScript: Story = {};
+export const SuperScript: Story = {
+    args: {
+        child: "SuperScript",
+    },
+};
 
-export const Link: Story = {};
+export const Link: Story = {
+    args: {
+        child: "Link",
+    },
+};
