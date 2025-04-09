32df081d7b4572db213e52a61f2e3491af830141
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-09 20:33:09 +0530

    MAJ-333 - Updates for maj-typography component.

---
+++ src/components/maj-typography/maj-heading-one/MajHeadingOne.tsx
@@ -0,0 +1,11 @@
+import { MajH1Props } from "./MajHeadingOne.types";
+
+const MajH1 = ({ children }: MajH1Props) => {
+    return (
+        <>
+            <h1>{children}</h1>
+        </>
+    );
+};
+
+export default MajH1;
