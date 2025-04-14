411d5eae8db0f020389587b8e144713a2c0f16fa
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-14 19:04:06 +0530

    MAJ-339 - Updates for maj-typography component.

--- src/components/maj-typography/MajTypography.tsx
+++ src/components/maj-typography/MajTypography.tsx
@@ -1,4 +1,5 @@
 import { MajH1 } from "./maj-heading-one";
+import { MajH3 } from "./maj-heading-three";
 import { MajH2 } from "./maj-heading-two";
 
 const MajTypography = () => {
@@ -6,7 +7,7 @@ const MajTypography = () => {
         <>
             <MajH1>H1 typography works!</MajH1>
             <MajH2>H2 typography works!</MajH2>
-            <h3>H3 typography works!</h3>
+            <MajH3>H3 typography works!</MajH3>
             <h4>H4 typography works!</h4>
             <h5>H5 typography works!</h5>
             <h6>H6 typography works!</h6>
---
+++ src/components/maj-typography/maj-heading-three/MajHeadingThree.types.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export type MajH3Props = {
+    children?: ReactNode;
+};
