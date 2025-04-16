f297e8b80ff1519a91b2322b1424f68e6241639c
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-16 22:29:59 +0530

    MAJ-341 - Updates for maj-typography componnet.'

---
+++ src/components/maj-typography/maj-heading-five/MajHeadingFive.tsx
@@ -0,0 +1,11 @@
+import { MajH5Props } from "./MajHeadingFive.types";
+
+const MajH5 = ({ children }: MajH5Props) => {
+    return (
+        <>
+            <h5>{children}</h5>
+        </>
+    );
+};
+
+export default MajH5;
