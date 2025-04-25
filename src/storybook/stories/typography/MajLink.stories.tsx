80452788bddeca524f8bb8b42458f1bef4de5c84
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-25 22:09:05 +0530

    MAJ-350 - Updates for maj-typography component.

---
+++ src/storybook/stories/typography/MajLink.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { MajLink } from "../../../components/maj-typography";
+
+const meta: Meta<typeof MajLink> = {
+    title: "MajUi/MajTypography/MajLink",
+    component: MajLink,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajLink>;
+
+export const Bold: Story = {
+    args: {
+        children: "Don't panic! It's a simple link.",
+    },
+};
