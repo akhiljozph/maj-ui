0aa582a010d6c454b84a454ba77acc456165a815
Akhil Joseph <ajpulluvelil@gmail.com>
2025-04-26 18:34:48 +0530

    MAJ-352 - Updates for maj-typoigraphy component.

---
+++ src/storybook/stories/typographies/MajSubScript.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { MajSubScript } from "../../../components/maj-typography";
+
+const meta: Meta<typeof MajSubScript> = {
+    title: "MajUi/MajTypography/MajSubScript",
+    component: MajSubScript,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajSubScript>;
+
+export const Bold: Story = {
+    args: {
+        children: "Be happy! It's a subscript boy.",
+    },
+};
