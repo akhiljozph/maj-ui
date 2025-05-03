239dfc15f15d5ed89b1b8683422c968cb0d4a10b
akhiljozph <ajpulluvelil@gmail.com>
2025-05-03 15:10:07 +0530

    MAJ-357 - Updates for maj-typography component.

---
+++ src/storybook/stories/typographies/MajHeadingFive.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { MajH5 } from "../../../components/maj-typography";
+
+const meta: Meta<typeof MajH5> = {
+    title: "MajUi/MajTypography/MajH5",
+    component: MajH5,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajH5>;
+
+export const Bold: Story = {
+    args: {
+        children: "Keep calm! This is a heading 5.",
+    },
+};
