aca6a732e2bf5a254d84dbfc806701115f50e7b5
akhiljozph <ajpulluvelil@gmail.com>
2025-05-13 23:31:30 +0530

    MAJ-371 - Updates for maj-typography component.

--- src/components/maj-typography/maj-label/MajLabel.tsx
+++ src/components/maj-typography/maj-label/MajLabel.tsx
@@ -1,11 +1,7 @@
 import { MajLabelProps } from "./MajLabel.types";
 
 const MajLink = ({ children }: MajLabelProps) => {
-    return (
-        <>
-            <label>{children}</label>
-        </>
-    );
+    return <label>{children}</label>;
 };
 
 export default MajLink;
