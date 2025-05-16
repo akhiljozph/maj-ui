d8c5bb7f9511d5a392eed453493981b01107b788
akhiljozph <ajpulluvelil@gmail.com>
2025-05-16 20:44:56 +0530

    MAJ-375 - Updates for maj-typography component.

---
+++ src/storybook/stories/typographies/MajArticle.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { MajArticle } from "../../../components/maj-typography";
+
+const meta: Meta<typeof MajArticle> = {
+    title: "MajUi/MajTypography/MajArticle",
+    component: MajArticle,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajArticle>;
+
+export const Article: Story = {
+    args: {
+        children: "Don't panic! It's a simple article.",
+    },
+};
