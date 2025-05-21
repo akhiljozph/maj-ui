f72ba31931d3c539f0f4cc4e5bbac19be63758b2
Akhil Joseph <ajpulluvelil@gmail.com>
2025-05-21 21:01:14 +0530

    MAJ-380 - Updates for maj-embed component.

--- src/components/maj-embed/MajEmbed.tsx
+++ src/components/maj-embed/MajEmbed.tsx
@@ -1,7 +1,14 @@
 import { MajEmbedProps } from "./MajEmbed.types";
 
 const MajEmbed = (props: MajEmbedProps) => {
-    return <embed src={props.src} />;
+    return (
+        <embed
+            src={props.src}
+            type={props?.type}
+            width={props?.width}
+            height={props?.height}
+        />
+    );
 };
 
 export default MajEmbed;
