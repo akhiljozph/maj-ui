3d024f209d5cefc6a3fc0127d6c672f330af16e9
Akhil Joseph <ajpulluvelil@gmail.com>
2025-06-06 22:21:55 +0530

    MAJ-390 - Updates for maj-card component.

--- src/components/maj-card/MajCard.tsx
+++ src/components/maj-card/MajCard.tsx
@@ -1,9 +1,14 @@
 import { MajCardProps } from "./MajCard.types";
 
 import "./MajCard.scss";
+import { MajArticle } from "../maj-article";
 
 const MajCard: React.FC<MajCardProps> = (props: MajCardProps) => {
-    return <>{props?.children}</>;
+    return (
+        <div>
+            <MajArticle>{props?.children}</MajArticle>
+        </div>
+    );
 };
 
 export default MajCard;
