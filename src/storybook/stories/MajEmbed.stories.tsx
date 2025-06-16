0add91b79c9ba63ebb64f931f056344ef0179f53
Akhil Joseph <ajpulluvelil@gmail.com>
2025-06-16 20:37:51 +0530

    MAJ-400 - Updates for maj-embed component.

--- src/storybook/stories/MajEmbed.stories.tsx
+++ src/storybook/stories/MajEmbed.stories.tsx
@@ -11,5 +11,10 @@ export default meta;
 type Story = StoryObj<typeof MajEmbed>;
 
 export const Embed: Story = {
-    args: {},
+    args: {
+        src: "https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=2048x2048&w=is&k=20&c=KTpY1O4d7-EuX-R_GR_44Upc-n9esJOZFpcqvA4CM0E=",
+        type: "image/jpg",
+        width: "300",
+        height: "300",
+    },
 };
