8569b9a61bec4f1e2f26b79c2e50cff6efc26d27
Akhil Joseph <ajpulluvelil@gmail.com>
2025-06-22 19:30:59 +0530

    MAJ-407 - Updates for maj-snackbar component.

---
+++ src/storybook/stories/MajSnackbar.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MajSnackbar from "../../components/maj-snackbar/MajSnackbar";
+import React from "react";
+
+const meta: Meta<typeof MajSnackbar> = {
+    title: "MajUi/MajSnackbar",
+    component: MajSnackbar,
+};
+
+export default meta;
+type Story = StoryObj<typeof MajSnackbar>;
+
+export const Snackbar: Story = {
+    args: {
+        children: <>Testing snackbar!</>,
+    },
+};
