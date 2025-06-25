e7a30c87b6428d30aaf456f13cdf89588115f108
Akhil Joseph <ajpulluvelil@gmail.com>
2025-06-25 23:04:13 +0530

    MAJ-410 - Updates for snackbar component.

--- src/components/maj-snackbar/MajSnackbar.tsx
+++ src/components/maj-snackbar/MajSnackbar.tsx
@@ -3,7 +3,11 @@ import { MajSnackbarProps } from "./MajSnackbar.types";
 import "./MajSnackbar.scss";
 
 const MajSnackbar: React.FC<MajSnackbarProps> = (props: MajSnackbarProps) => {
-    return <div>{props?.children}</div>;
+    // return <div>{props?.children}</div>;
+    <></>;
+    console.log(props);
+
+    return "";
 };
 
 export default MajSnackbar;
