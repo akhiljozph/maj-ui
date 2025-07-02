b726ca21c2d314d1e7f49f3ab204d7c3f7e2bd24
Akhil Joseph <ajpulluvelil@gmail.com>
2025-07-02 22:32:59 +0530

    MAJ-417 - Update for maj project.

---
+++ storybook-static/assets/MajAccordion.stories-D4l5X0oO.js
@@ -0,0 +1,8 @@
+import{j as a}from"./jsx-runtime-DEdD30eg.js";import{M as s,a as t}from"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const i=e=>a.jsx("div",{children:e==null?void 0:e.children});i.__docgenInfo={description:"",methods:[],displayName:"MajAccordion",props:{children:{required:!1,tsType:{name:"ReactNode"},description:""}}};const j={title:"MajUi/MajAccordion",component:i},r={args:{children:a.jsxs(s,{children:["Maj SubScript Component",a.jsx(t,{children:"This is an example of subscript tag."})]})}};var n,o,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: <MajP>\r
+                Maj SubScript Component\r
+                <MajSubScript>This is an example of subscript tag.</MajSubScript>\r
+            </MajP>
+  }
+}`,...(c=(o=r.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const l=["Accordion"];export{r as Accordion,l as __namedExportsOrder,j as default};
