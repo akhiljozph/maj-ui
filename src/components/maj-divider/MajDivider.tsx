1ea0d38f776140b5432d41aca92b05fe701582a4
Akhil Joseph <ajpulluvelil@gmail.com>
2025-07-21 22:18:49 +0530

    MAJ-436 - Updates for maj-divider.

--- src/components/maj-divider/MajDivider.tsx
+++ src/components/maj-divider/MajDivider.tsx
@@ -1,9 +1,25 @@
-// import { MajDividerProps } from "./MajDivider.types";
+import { MajDividerProps } from "./MajDivider.types";
 
 import "./MajDivider.scss";
 
-const MajDivider = () => {
-    return <div className="maj-divider"></div>;
+const MajDivider = (props: MajDividerProps) => {
+    let classnames: string;
+
+    switch (props?.variant) {
+        case "dashed":
+            classnames = "maj-divider dashed-divider";
+            break;
+        case "dotted":
+            classnames = "maj-divider dotted-divider";
+            break;
+        case "solid":
+            classnames = "maj-divider solid-divider";
+            break;
+        default:
+            classnames = "maj-divider solid-divider";
+            break;
+    }
+    return <div className={classnames}></div>;
 };
 
 export default MajDivider;
