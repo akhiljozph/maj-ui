43fddeffe2551fec97c7dec05702fcbd35ded037
Akhil Joseph <ajpulluvelil@gmail.com>
2025-07-23 23:49:18 +0530

    MAJ-438 - Storybook build.

--- storybook-static/assets/MajDivider.stories-f3EdUP3X.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const o=a=>e.jsxs(e.Fragment,{children:[a," maj-divider works!"]});o.__docgenInfo={description:"",methods:[],displayName:"MajDivider",props:{stroke:{required:!0,tsType:{name:"string"},description:""}}};const d={title:"MajUi/MajDivider",component:o},r={args:{stroke:"12"}};var t,s,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    stroke: "12"
-  }
-}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const p=["ArticleWithSubScript"];export{r as ArticleWithSubScript,p as __namedExportsOrder,d as default};
