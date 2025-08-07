c2f8d3f0e11ee27f61c835c846f1bf3ec77b68fd
Akhil Joseph <ajpulluvelil@gmail.com>
2025-08-07 23:16:34 +0530

    MAJ-456 - Updates for maj-accordion.

--- src/components/maj-accordion/MajAccordion.tsx
+++ src/components/maj-accordion/MajAccordion.tsx
@@ -1,13 +1,14 @@
 import { MajAccordionProps } from "./MajAccordion.types";
 
 import "./MajAccordion.scss";
+import { MajSection } from "../index";
 
 const MajAccordion: React.FC<MajAccordionProps> = (props: MajAccordionProps) => {
     return (
-        <>
+        <MajSection>
             <button>{props?.accordionTitle}</button>
             <div>{props?.children}</div>
-        </>
+        </MajSection>
     );
 };
 
