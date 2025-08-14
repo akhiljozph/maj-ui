b3beedfc2b6a726255ae49374e49793bc5902849
Akhil Joseph <ajpulluvelil@gmail.com>
2025-08-14 22:44:22 +0530

    MAJ-465 - Updates for maj-accordian.

---
+++ storybook-static/assets/MajHeader.stories-C3h6aw0B.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{n as s,a as i}from"./MajTooltip-w1zKxDgI.js";import"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const d={title:"MajUi/MajHeader",component:s},r={args:{children:t.jsx(i,{children:"Hello world!"})}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: <MajArticle>Hello world!</MajArticle>
+  }
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const p=["Primary"];export{r as Primary,p as __namedExportsOrder,d as default};
---
+++ storybook-static/assets/MajProgressBar.stories-HrX5bKyP.js
@@ -0,0 +1,5 @@
+import{s as o}from"./MajTooltip-w1zKxDgI.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const c={title:"MajUi/MajProgressBar",component:o},r={args:{message:"progress-bar"}};var s,a,e;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    message: "progress-bar"
+  }
+}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const i=["Primary"];export{r as Primary,i as __namedExportsOrder,c as default};
