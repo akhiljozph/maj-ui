12c139165b881ed9ae9ee7c2241496e2f1ee5006
Akhil Joseph <ajpulluvelil@gmail.com>
2025-08-20 22:30:48 +0530

    MAJ-472 - Updates for maj accordian component.

---
+++ storybook-static/assets/MajSelect.stories-Cdq-nnes.js
@@ -0,0 +1,17 @@
+import{w as t}from"./MajTooltip-zw7VdAIH.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const s={title:"MajUi/MajSelect",component:t},n={args:{options:[{name:"Option 1",value:1},{name:"Option 2",value:2},{name:"Option 3",value:3},{name:"Option 4",value:4}]}};var e,a,o;n.parameters={...n.parameters,docs:{...(e=n.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      name: "Option 1",
+      value: 1
+    }, {
+      name: "Option 2",
+      value: 2
+    }, {
+      name: "Option 3",
+      value: 3
+    }, {
+      name: "Option 4",
+      value: 4
+    }]
+  }
+}`,...(o=(a=n.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const l=["Primary"];export{n as Primary,l as __namedExportsOrder,s as default};
