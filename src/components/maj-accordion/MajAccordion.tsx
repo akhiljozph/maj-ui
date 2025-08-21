15b1308a14ed6c05b5ca73f975d3101032b25933
Akhil Joseph <ajpulluvelil@gmail.com>
2025-08-21 22:39:37 +0530

    MAJ-475 - Updates for maj-accordion component.

--- src/components/maj-accordion/MajAccordion.tsx
+++ src/components/maj-accordion/MajAccordion.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { MajAccordionProps } from "./MajAccordion.types";
-import { MajP } from "../maj-typography";
+import { MajP, MajSubScript } from "../maj-typography";
 
 import "./MajAccordion.scss";
 
@@ -13,18 +13,27 @@ const MajAccordion: React.FC<MajAccordionProps> = (props: MajAccordionProps) => 
     }
 
     return (
-        <div className="maj-accordion-wrapper">
-            <button
-                className="accordion"
-                style={props?.accordionTitleStyle}
-                onClick={onAccordionTitleClick}
-            >
-                {props?.accordionTitle}
-            </button>
-            <div className={`panel ${isOpen ? "open" : ""}`}>
-                <MajP>{props?.children}</MajP>
+        <>
+            <div className="maj-accordion-wrapper">
+                <button
+                    className="accordion-title-wrapper"
+                    style={props?.accordionTitleStyle}
+                    onClick={onAccordionTitleClick}
+                >
+                    <label className="accordion-title">{props?.accordionTitle}</label>
+                    <span className="accordion-icon">
+                        {isOpen ? (
+                            <MajSubScript>&#x2212;</MajSubScript>
+                        ) : (
+                            <MajSubScript>&#x2b;</MajSubScript>
+                        )}
+                    </span>
+                </button>
+                <div className={`accordion-panel ${isOpen ? "accordion-expanded" : ""}`}>
+                    <MajP>{props?.children}</MajP>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
