9383ee55a1c77a32b59d59b1eaf05a2bfbad2ac6
Akhil Joseph <ajpulluvelil@gmail.com>
2025-08-23 11:57:19 +0530

    MAJ-477 - Updates for maj-accordion component.

---
+++ storybook-static/assets/MajAccordion.stories-z3-gV3EL.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{M as s}from"./MajTooltip-CGt7-av8.js";import{M as e}from"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const d={title:"MajUi/MajAccordion",component:s},t={args:{items:[{title:"Section 1",content:n.jsx(e,{children:"This is the content of section 1. It can include text, images, or any other React components."})},{title:"Section 2",content:n.jsx(e,{children:"This is the content of section 2. It can include text, images, or any other React components."})},{title:"Section 3",content:n.jsx(e,{children:"This is the content of section 3. It can include text, images, or any other React components."})}]}};var o,c,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      title: "Section 1",
+      content: <MajP>\r
+                        This is the content of section 1. It can include text, images, or any other\r
+                        React components.\r
+                    </MajP>
+    }, {
+      title: "Section 2",
+      content: <MajP>\r
+                        This is the content of section 2. It can include text, images, or any other\r
+                        React components.\r
+                    </MajP>
+    }, {
+      title: "Section 3",
+      content: <MajP>\r
+                        This is the content of section 3. It can include text, images, or any other\r
+                        React components.\r
+                    </MajP>
+    }]
+  }
+}`,...(i=(c=t.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const l=["Accordion"];export{t as Accordion,l as __namedExportsOrder,d as default};
--- storybook-static/assets/MajSwitch.stories-NvIeO3hJ.js
+++
@@ -1,3 +0,0 @@
-import{y as o}from"./MajTooltip-DdFZzcN1.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const i={title:"MajUi/MajSwitch",component:o},r={args:{}};var t,a,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {}
-}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const n=["Default"];export{r as Default,n as __namedExportsOrder,i as default};
