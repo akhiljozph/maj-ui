7bac14ad897bb2ff751dd324d0cccee0b1fbde10
Akhil Joseph <ajpulluvelil@gmail.com>
2025-09-06 16:18:39 +0530

    MAJ-488 - Verifies storybook build.

---
+++ storybook-static/assets/MajDivider.stories-D506ctu8.js
@@ -0,0 +1,31 @@
+import{l as f}from"./MajTooltip-p70uADOI.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-Bph0vahL.js";import"./index-RYns6xqu.js";const b={title:"MajUi/MajDivider",component:f},r={args:{stroke:"12",variant:"solid"}},e={args:{stroke:"12",variant:"dotted"}},a={args:{stroke:"12",variant:"dashed"}},s={args:{stroke:"12",variant:"solid"}},t={args:{stroke:"12",variant:"dotted"}},o={args:{stroke:"12",variant:"dashed"}};var i,n,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "solid"
+  }
+}`,...(d=(n=r.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};var c,m,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "dotted"
+  }
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var v,u,g;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "dashed"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var D,l,k;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "solid"
+  }
+}`,...(k=(l=s.parameters)==null?void 0:l.docs)==null?void 0:k.source}}};var S,O,h;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "dotted"
+  }
+}`,...(h=(O=t.parameters)==null?void 0:O.docs)==null?void 0:h.source}}};var j,E,M;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    stroke: "12",
+    variant: "dashed"
+  }
+}`,...(M=(E=o.parameters)==null?void 0:E.docs)==null?void 0:M.source}}};const q=["SolidDivider","DottedDivider","DashedDivider","DividerOrientationStart","DividerOrientationCenter","DividerOrientationEnd"];export{a as DashedDivider,t as DividerOrientationCenter,o as DividerOrientationEnd,s as DividerOrientationStart,e as DottedDivider,r as SolidDivider,q as __namedExportsOrder,b as default};
--- storybook-static/components/maj-article/MajArticle.types.d.ts
+++ storybook-static/components/maj-article/MajArticle.types.d.ts
@@ -1,4 +1,5 @@
 import { ReactNode } from "../../../node_modules/react";
 export type MajArticleProps = {
+    className?: string;
     children?: ReactNode;
 };
