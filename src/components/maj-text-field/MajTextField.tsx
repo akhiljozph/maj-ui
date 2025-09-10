85996ffc28816684066c508d088f39fe7d536a44
Akhil Joseph <ajpulluvelil@gmail.com>
2025-09-10 22:01:52 +0530

    MAJ-492 - Updates for maj-text-field and maj-tooltip.

--- src/components/maj-text-field/MajTextField.tsx
+++ src/components/maj-text-field/MajTextField.tsx
@@ -1,16 +1,17 @@
 import { MajTextFieldProps } from "./MajTextField.types";
 
 import "./MajTextField.scss";
+import { MajSection } from "../maj-section";
 
 const MajTextField: React.FC<MajTextFieldProps> = (props: MajTextFieldProps) => {
     return (
-        <section>
+        <MajSection>
             <textarea
                 rows={props.rows ? props.rows : 10}
                 cols={props.cols ? props.cols : 10}
                 placeholder={props.placeholder ? props.placeholder : "Description please..."}
             ></textarea>
-        </section>
+        </MajSection>
     );
 };
 
---
+++ storybook-static/assets/MajButtonGroup.stories-Cob1mpw0.js
@@ -0,0 +1,3 @@
+import{f as e}from"./MajTooltip-B8zIMogL.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-CmSdUy0f.js";import"./index-RYns6xqu.js";const c={title:"MajUi/MajButtonGroup",component:e},r={args:{}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {}
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Primary"];export{r as Primary,i as __namedExportsOrder,c as default};
