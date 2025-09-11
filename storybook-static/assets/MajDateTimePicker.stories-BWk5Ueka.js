4060ae4b5937bff2bd2246692ef2666713c212f2
Akhil Joseph <ajpulluvelil@gmail.com>
2025-09-11 22:31:08 +0530

    MAJ_493 - Updates for maj-switch.

---
+++ storybook-static/assets/MajDateTimePicker.stories-BWk5Ueka.js
@@ -0,0 +1,11 @@
+import{k as p}from"./MajTooltip-DMO3x9Pz.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-CmSdUy0f.js";import"./index-RYns6xqu.js";const O={title:"MajUi/MajDateTimePicker",component:p},e={args:{}},a={args:{minimumValue:new Date().toISOString().slice(0,16)}},r={args:{maximumValue:new Date().toISOString().slice(0,16)}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var i,m,n;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    minimumValue: new Date().toISOString().slice(0, 16)
+  }
+}`,...(n=(m=a.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var c,u,l;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    maximumValue: new Date().toISOString().slice(0, 16)
+  }
+}`,...(l=(u=r.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};const b=["Default","PreviousDatesDisabled","FutureDatesDisabled"];export{e as Default,r as FutureDatesDisabled,a as PreviousDatesDisabled,b as __namedExportsOrder,O as default};
---
+++ storybook-static/assets/MajSlider.stories-B_0gnFz-.js
@@ -0,0 +1,9 @@
+import{x as m}from"./MajTooltip-DMO3x9Pz.js";import"./jsx-runtime-DEdD30eg.js";import"./MajSuperScript-CmSdUy0f.js";import"./index-RYns6xqu.js";const l={title:"MajUi/MajSlider",component:m},r={args:{min:0,max:100,label:"Volume",step:5,value:45}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    min: 0,
+    max: 100,
+    label: "Volume",
+    step: 5,
+    value: 45
+  }
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const p=["Primary"];export{r as Primary,p as __namedExportsOrder,l as default};
